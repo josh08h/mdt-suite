b6c259507e4e54ed04e6bcbac60847738a1280dc
Josh Hale <hale.j.0810@gmail.com>
2016-08-05 19:18:36 +0100

    d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './App';
@@ -7,7 +7,7 @@ import App from './App';
 
 const store = configureStore();
 
-ReactDOM.render(
+render(
 	<Provider store={store}>
   	<App />
   </Provider>,
